eb6c551286af80126fc41d9b60d1d13e53324c01
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-31 15:12:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
 
   get '/square/:number' do
       @number = params[:number].to_i
-      @number_sq @number ** 2
+      @number_sq = @number ** 2
   end
 
   get '/say/:number/:phrase' do
